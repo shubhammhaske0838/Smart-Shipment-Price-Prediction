a6c3206b9d7c737a852281ca451a554bbf9a0709
shubham mhaske <shubham.mhaske0838@gmail.com>
2023-10-15 12:05:32 +0530

    Updated template.py file

--- template.py
+++ template.py
@@ -36,7 +36,7 @@ for filepath in list_of_files:
     if filedir != '':
         os.makedirs(filedir, exist_ok = True)
 
-    if ((not os.path.exists(filepath)) or (os.path.getsize == 0 )):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0 ):
         with open (filepath, 'w') as f:
             pass
 
